313c77773e25f079e97970932acd9b14e5e13fcc
Aaron Nojima <aanojima@mit.edu>
2014-03-19 18:32:05 +0100

    First Commit

---
+++ game-server.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+var GameManager = require('../node/game_manager').GameManager;
+
+var server = http.createServer(function(request, response){
+	console.log((new Date()) + ' Received request for ' + request.url);
+	response.writeHead(404);
+	response.end();
+});
+
+console.log(server);
+
+server.listen(8080, function() {
+	console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+	httpServer : server,
+	autoAcceptConnections : false,
+});
+
+function originIsAllowed(origin) {
+	return true;
+}
+
+var gm = new GameManager(4, wsServer);
